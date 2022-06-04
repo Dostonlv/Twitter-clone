ffa9d89d1316791d2ac8c55b93dc9878d379b4fd
Doston <IamitachiUchixa@gmail.com>
2022-06-04 11:16:21 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'twitter-blue': '#00acee'
+      }
+    }
+  },
+  plugins: [],
+}
